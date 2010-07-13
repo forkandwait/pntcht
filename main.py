eca8673bd7a39324ca39def30037752024334810
W. Webb Sprague (OFM) <webb.sprague@ofm.wa.gov>
2010-07-13 11:33:01 -0700

    Basic prototype now working.  Stupid simplejson.

--- main.py
+++ main.py
@@ -23,7 +23,13 @@ class Building(db.Model):
     pt = db.GeoPtProperty()
     inst = db.ReferenceProperty(Institution)
 
-	
+
+class Comment(db.Model):
+    msg = db.StringProperty()
+    inst = db.ReferenceProperty(Institution)
+    timestamp = db.DateTimeProperty(auto_now=True)
+    user = db.UserProperty(auto_current_user=True)
+    
 class MainPage(webapp.RequestHandler):
     def get(self):
         if users.get_current_user():
@@ -50,31 +56,41 @@ class UploadInst(webapp.RequestHandler):
         if users.get_current_user():
             logging.debug(users.get_current_user().nickname())
             pass
-        
-        inst_name = self.request.get('inst_name')
-        inst_rec = db.GqlQuery("SELECT * FROM Institution WHERE name = :1", inst_name).fetch(1)
-        
-        ## store institution
-        inst = Institution()
-        inst.name = inst_name
-        inst.unguessable_id = str(uuid.uuid4())
-        inst.put()
+        try:
+            ## store institution
+            inst = Institution()
+            inst.name = self.request.get('inst_name')
+            inst.unguessable_id = str(uuid.uuid4())
+            inst.put()
 
-        ## store buildings
-        bldg_table_str = str(self.request.get('bldg_table'))
-        for line in re.split("\n+", bldg_table_str):
-            line2 = line.strip()
-            elems = re.split("\s+", line2, 2)
-            bldg = Building(
-                inst = inst.key(),
-                pt = db.GeoPt(float(elems[0]), float(elems[1])),
-                name = elems[2])
-            bldg.put()
+            ## store first comment
+            comm = Comment()
+            comm.msg = "Initial upload.  " + self.request.get('comment')
+            comm.inst = inst.key()
+            comm.put()
+
+            ## store buildings
+            bldg_table_str = str(self.request.get('bldg_table')).strip()
+            for line in re.split("\n+", bldg_table_str):
+                line2 = line.strip()
+                elems = re.split("\s+", line2, 2)
+                bldg = Building(
+                    inst = inst.key(),
+                    pt = db.GeoPt(float(elems[0]), float(elems[1])),
+                    name = elems[2])
+                bldg.put()
+                pass
+
+            ## view the results (redirect)
+            self.redirect('/view?unguessable_id=%s' % inst.unguessable_id)
             pass
         
-        ## view the results
-        self.redirect('/view?unguessable_id=%s' % inst.unguessable_id)
-        pass
+        except Exception, exc:
+            error_msg = "There was a problem parsing input data.  Python message: \"%s\".<br>  Press <b>back button</b> and fix." % exc
+            template_values = {"error_msg":error_msg}
+            path = os.path.join(os.path.dirname(__file__), 'templates/error.tmpl')
+            self.response.out.write(template.render(path, template_values))
+            pass
 
 
 class ViewInst(webapp.RequestHandler):
@@ -82,23 +98,21 @@ class ViewInst(webapp.RequestHandler):
         if users.get_current_user():
             logging.debug(users.get_current_user().nickname())
 
-        ## get all buildings for institution's id, pass to a template, write
+        ## get all buildings + comments for institution's id
         unguessable_id = self.request.get('unguessable_id')
-        if not unguessable_id:
-            self.redirect('/') 
-            return
         sqlstr = "select *  from Institution where unguessable_id = :1"
         query = db.GqlQuery(sqlstr, unguessable_id)
-        
-        if query.count() != 1:
+        if (query.count() != 1) or (not unguessable_id):
             logging.error("ViewInst.get: bad number records returned for inst: %s" % query.count())
             self.redirect('/') 
             return
         inst = query.fetch(query.count())[0]
-        bldg_table = inst.building_set
+        
+        ## pass to template and write
         template_values = {'unguessable_id':inst.unguessable_id,
                            'inst_name':inst.name,
-                           'bldg_table':bldg_table}
+                           'comments':inst.comment_set,
+                           'bldg_table':inst.building_set}
         path = os.path.join(os.path.dirname(__file__), 'templates/view.tmpl')
         self.response.out.write(template.render(path, template_values))
 
@@ -106,21 +120,26 @@ class ViewInst(webapp.RequestHandler):
 class UpdateInst(webapp.RequestHandler):
     ## Update positions of institution, using JSON from AJAX
     def post(self):
+        ## params
         unguessable_id = self.request.get('unguessable_id')
-        modeljson = self.request.get('model')
-        model = simplejson.loads(modeljson)
+        model = simplejson.loads(self.request.get('model'))
+        comm_msg = self.request.get('comment')
+
+        ## update buildings
         instq = db.GqlQuery("SELECT * FROM Institution WHERE unguessable_id = :1", unguessable_id)
         inst = instq.fetch(instq.count())[0]
-        if inst: 
-            for bldg in inst.building_set: 
-                a,b = model[bldg.name]['pt'].strip('()').split(', ')
-                bldg.pt = db.GeoPt(float(a), float(b))
-                bldg.put()
-                pass
-        else:
-            logging.error("UpdateInst.post: no institution returned from query")
+        for bldg in inst.building_set: 
+            a,b = model[bldg.name]['latlong'].strip('()').split(', ')
+            logging.debug("UpdateInst.post():updating building: %s. (%s, %s)." % (bldg.name, a, b))
+            bldg.pt = db.GeoPt(float(a), float(b))
+            bldg.put()
             pass
-        
+
+        ## add comment
+        comm = Comment()
+        comm.msg = "Points updated.  " + comm_msg
+        comm.inst = inst.key()
+        comm.put()
 
 ######
 application = webapp.WSGIApplication([('/', MainPage),
