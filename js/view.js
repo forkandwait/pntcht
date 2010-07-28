1a9a09bc61b3f07372da68002389ee62812f29d4
W. Webb Sprague (OFM) <webb.sprague@ofm.wa.gov>
2010-07-28 12:04:54 -0700

    Continuing to make more modular, separating js ...

--- js/view.js
+++ js/view.js
@@ -9,7 +9,6 @@ function updateMarker (event) {
   $(sel_lat).html( this.getPosition().lat().toString());
   var sel_lon = "#point_table #" + this.title + " #bldg_lon";
   $(sel_lon).html( this.getPosition().lng().toString());
-
 }
 	
 function updateFacilities(FACILITIES_MODEL, UNGUESSABLE_ID, comment) {
@@ -29,13 +28,36 @@ function updateFacilities(FACILITIES_MODEL, UNGUESSABLE_ID, comment) {
   $.ajax(aj_params);
 }
 
-function make_point(bldg_name, lat, long, map, bounds, FACILITIES_MODEL) {
+function make_point(bldg_name, lat, long, gmap, bounds, FACILITIES_MODEL) {
   var name = bldg_name;
   var myLatLong = new google.maps.LatLng(lat, long);
   var marker = new google.maps.Marker({position: myLatLong, title:bldg_name});
   marker.setDraggable(true);
-  marker.setMap(map); 
+  marker.setMap(gmap); 
   google.maps.event.addListener(marker, 'mouseup', updateMarker);
   bounds.extend(myLatLong);
   FACILITIES_MODEL[name] = {'name':name, 'latlong':(myLatLong.toString())};			
 }
+
+
+function make_map_xml(FMODEL, gmap, markers_xml) {
+  // TODO -- make this grab from a KML feed rather than generated code (blech!) 
+  
+  gmap = new google.maps.Map(document.getElementById("map_canvas"), {mapTypeId: google.maps.MapTypeId.ROADMAP});
+  
+  // Facilities markers -- generate a bunch of JS 
+  //    via django template loop, store array globally.
+  var name = "";
+  var myLatLong = "";
+  var marker = "";
+  var bounds = new google.maps.LatLngBounds();
+  //{% for bldg in bldg_table %}
+  //make_point("{{ bldg.name }}", {{ bldg.pt }}, gmap, bounds, fmodel);  // XXX bldg.pt expands 
+  //{% Endfor %}
+  
+  // Zoom
+  gmap.setCenter(bounds.getCenter());
+  gmap.setZoom(14); 
+}
+
+
